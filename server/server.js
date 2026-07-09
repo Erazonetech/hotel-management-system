582f988d5620eee6759c8b351729d5cffb08ff82
Amanuel Tamirat <Amanuel_Tamirat@sch.local>
2026-07-09 09:12:53 +0300

    cors

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ import http from 'http';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
-
+import cors from 'cors'
 // Get __dirname equivalent in ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -34,11 +34,12 @@ import tableRoutes from './routes/tableRoutes.js';
 import analyticsRoutes from './routes/analyticsRoutes.js';
 import paymentRoutes from './routes/paymentRoutes.js';
 
+
 // Initialize express app
 const app = express();
 const server = http.createServer(app);
 
-const cors = require("cors");
+
 
 const allowedOrigins = [
   "https://sigmatradinget.com",
